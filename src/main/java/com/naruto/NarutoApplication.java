cc23469b90ca8149403763f3a5fc21d30a108427
yunuotianming <yunuotianming@163.com>
2019-06-02 18:49:24 +0800

    initialization mybatis

---
+++ src/main/java/com/naruto/NarutoApplication.java
@@ -0,0 +1,15 @@
+package com.naruto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class NarutoApplication {
+    public static void main( String[] args ){
+        SpringApplication.run(NarutoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/naruto/NarutoTestApplication.java
@@ -0,0 +1,20 @@
+package com.naruto;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit testFilter for simple App.
+ */
+public class NarutoTestApplication
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
