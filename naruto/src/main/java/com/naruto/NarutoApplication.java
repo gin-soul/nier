0fca16dee8e638813d17113d736fdd21ffaae9a8
gin <yunuotianming@163.com>
2020-02-12 13:12:45 +0800

    ts index

---
+++ naruto/src/main/java/com/naruto/NarutoApplication.java
@@ -0,0 +1,32 @@
+package com.naruto;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * CommandLineRunner、ApplicationRunner 接口是在容器启动成功后的最后一步回调（类似开机自启动）
+ * 此处使用 ApplicationRunner 是为了在 springboot 启动后测试下log4j2
+ *
+ */
+@SpringBootApplication
+//public class NarutoApplication implements ApplicationRunner {
+public class NarutoApplication {
+    private static final Logger logger = LogManager.getLogger(NarutoApplication.class);
+
+    public static void main( String[] args ){
+        SpringApplication.run(NarutoApplication.class, args);
+    }
+/*
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        logger.debug("Debugging log");
+        logger.info("Info log");
+        logger.warn("Hey, This is a warning!");
+        logger.error("Oops! We have an Error. OK");
+        logger.fatal("Damn! Fatal error. Please fix me.");
+    }*/
+}
